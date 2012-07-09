c12d76ca86d2be918c2f02f7083566469d3771bc
Vivek Harikrishnan <r.vivekharikrishnan@gmail.com>
2012-07-09 12:58:08 -0400

    Finish layouts and routes

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,10 @@
 module StaticPagesHelper
+	def full_title(title)
+		basic_title = "Sample Application"
+		if title.blank?
+			basic_title
+		else
+			"#{basic_title} | #{title}"
+		end
+	end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,17 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
 require 'rspec/autorun'
-require 'capybara/rspec'
+require 'rspec/rails'
+require 'webrat'
+require 'webmock/rspec'
 
+WebMock.disable_net_connect!(:allow_localhost => true)
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+require 'webrat/integrations/rspec-rails'
 
 RSpec.configure do |config|
   # ## Mock Framework
@@ -17,6 +21,8 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+    config.mock_with :rr
+    config.include WebMock
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
