7aeef1084cf84fa03d67c0de985c67b246443d14
Vivek Harikrishnan <r.vivekharikrishnan@gmail.com>
2012-07-12 13:05:31 -0400

    Redirection is updated after signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_back_or user
+			redirect_back_or root_path
 		else
 			flash[:error] = "Invalid email/password combination"
 			render 'new'
